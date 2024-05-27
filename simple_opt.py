7ada27c6314e51266e97435b02e65b4d0df2c639
Michelle Ho <mtho@princeton.edu>
2024-05-26 19:06:38 -0700

    update optimization

--- simple_opt.py
+++ simple_opt.py
@@ -6,7 +6,19 @@ def rosen(x):
     return sum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0)
 
 x0 = np.array([1.3, 0.7, 0.8, 1.9, 1.2])
-res = minimize(rosen, x0, method='nelder-mead',
-               options={'xatol': 1e-8, 'disp': True})
+for method in ['Nelder-Mead', 'Powell', 'CG', 'BFGS', 'L-BFGS-B', 'TNC', 'COBYLA', 'SLSQP']:
+    for i in range(10):
+        res = minimize(rosen, x0, method=method,
+                options={'xatol': 1e-8, 'disp': True})
+        if res.success:
+            print(method, res.x)
+            break
 
-print(res.x)
+# returns x- answer, success 
+# - bool if optimization was successful, 
+# status - int, 
+# message - str, 
+# fun, jas, hess - value of the function at the minimum, 
+# nfev - number of function evaluations, 
+# nit - number of iterations. 
+# maxcv - max constraint violation
