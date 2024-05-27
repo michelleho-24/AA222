f27dee66f6268a497394b67ed83c50374a4ce386
kseefried <seefried@stanford.edu>
2024-05-26 18:18:39 -0700

    new final function

---
+++ final_function.py
@@ -0,0 +1,15 @@
+# This is where the final function for optimization will live, as well as the initial conditions 
+def satellite_lost(n,m):
+    # intialize orbital elements for n sastellites for evenly spaced constellation
+    # delete m satellites so there are n-m satellites left
+    # append heights and inclinations to input vector as the starting point
+
+    # initialize optimization problem using optimization package of choice 
+    # use calculate_surface_area as objective function
+
+
+
+def calculate_surface_area(vec): 
+    # inputs will be a vector of altitudes and inclinations for n satellites 
+    # output will be surface area coverage compared to original coverage, original coverage initialized before loop
+    
